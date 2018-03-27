df10d408746345c5e1bbfdbdee286158f1c6bb20
Nicu <timoftealexandru5@gmail.com>
2018-03-27 14:45:31 +0300

    added repo

---
+++ src/main/java/lab2_ssvv/App.java
@@ -0,0 +1,13 @@
+package lab2_ssvv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
